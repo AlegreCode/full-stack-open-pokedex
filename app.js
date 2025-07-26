71a3f96c30375c25b975f514f4cf1bd208ac289b
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2025-07-26 19:59:16 -0300

    fix: update eslint set up

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-import express from "express";
-const app = express();
+import express from 'express'
+const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+/* global process */
+const PORT = process.env.PORT || 5000
 
-app.use(app.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+  // eslint-disable-next-line no-console
+  console.log(`server started on port ${PORT}`)
+})
